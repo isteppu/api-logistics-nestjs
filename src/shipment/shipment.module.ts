44617d925eaabed11754ffb929af6de28b4ca46b
isteppu <stefanieasaytono@gmail.com>
2026-02-09 14:33:32 +0800

    Fix shipment module

--- src/shipment/shipment.module.ts
+++ src/shipment/shipment.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ShipmentService } from './shipment.service.js';
 import { ShipmentResolver } from './shipment.resolver.js';
+import { ShipmentFinanceService } from './shipment-finance.service.js';
+import { PrismaService } from '../prisma/prisma.service.js';
 
 @Module({
-  providers: [ShipmentResolver, ShipmentService],
+  providers: [
+    ShipmentResolver, 
+    ShipmentService, 
+    ShipmentFinanceService, // 2. Add it here!
+    PrismaService
+  ],
 })
 export class ShipmentModule {}
